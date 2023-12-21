89d4b5ec7d72e1be2e49b91614b454627b86a2be
DavidReinon <davidreinongarcia@gmail.com>
2023-12-21 19:23:35 +0100

    rename pantallas

--- Ejercicios10Audio/src/screens/Ejercicio2.js
+++ Ejercicios10Audio/src/screens/Ejercicio2.js
@@ -1,15 +1,15 @@
 import { createStackNavigator } from "@react-navigation/stack";
 import { ScreensProvider } from "./ScreensContext";
-import Tab2_1 from "./Stack2_1";
-import Tab2_2 from "./Stack2_2";
+import Stack2_1 from "./Stack2_1";
+import Stack2_2 from "./Stack2_2";
 
 const Stack = createStackNavigator();
 
 const Ejercicio2 = () => (
     <ScreensProvider>
         <Stack.Navigator>
-            <Stack.Screen name="Tab2_1" component={Tab2_1} />
-            <Stack.Screen name="Tab2_2" component={Tab2_2} />
+            <Stack.Screen name="Stack2_1" component={Stack2_1} />
+            <Stack.Screen name="Stack2_2" component={Stack2_2} />
         </Stack.Navigator>
     </ScreensProvider>
 );
