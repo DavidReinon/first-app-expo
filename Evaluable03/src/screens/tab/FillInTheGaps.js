8dbf4e277bd6b1c0834bf03a6b0dd482a1c1a4e8
DavidReinon <davidreinongarcia@gmail.com>
2024-01-16 01:31:38 +0100

    improve: make the code more maintainable

--- Evaluable03/src/screens/tab/FillInTheGaps.js
+++ Evaluable03/src/screens/tab/FillInTheGaps.js
@@ -1,79 +1,31 @@
 import { TouchableOpacity, Text, TextInput, View } from "react-native";
-import { useEffect, useState } from "react";
-import playLocalSound, { getData, playUriSound } from "../../services/Services";
+import React, { useEffect, useState } from "react";
+import { getData, playUriSound } from "../../services/Services";
+import * as allData from "../../utils/fill_in_the_gaps.json";
+const data = allData[0];
 
 export default function FillInTheGaps() {
-    const data = require("../../utils/fill_in_the_gaps.json");
     const urlApi = "https://api.dictionaryapi.dev/api/v2/entries/en/";
-    const levelOne = data[0].levelOne;
-    const levelTwo = data[0].levelTwo;
+    const maxTries = 3;
+    const levelTranslator = {
+        1: data.levelOne,
+        2: data.levelTwo,
+    };
+    const levels = Object.keys(levelTranslator)
+        .map((level) => Number(level))
+        .sort((a, b) => a - b);
 
     const [tries, setTries] = useState(0);
-    const [level, setLevel] = useState(0);
+    const [level, setLevel] = useState(1);
     const [sentence, setSentence] = useState(null);
-    const [adjective, setAdjective] = useState("");
-    const [userAnswer, setUserAnswer] = useState("");
-
-    const randomNumber = (number) => {
-        if (Number(number) === 1) {
-            return Math.floor(Math.random() * levelOne.adjectives.length); //0-5
-        }
-        return Math.floor(Math.random() * levelTwo.adjectives.length); //0-7
-    };
-
-    const generateLevel = (number) => {
-        const random = randomNumber(number);
-        if (number === 1) {
-            setLevel(number);
-            setAdjective(levelOne.adjectives[random]);
-            setSentence(levelOne.sentences[random]);
-            return;
-        }
-        if (number === 2) {
-            setLevel(number);
-            setAdjective(levelTwo.adjectives[random]);
-            setSentence(levelTwo.sentences[random]);
-            return;
-        }
-    };
-    const correctAnswer = () => {
-        if (adjective === userAnswer.toLowerCase()) {
-            if (level === 2) {
-                alert("You WIN!!, Try Again");
-                setTries(0);
-                generateLevel(1);
-                setLevel(1);
-                setUserAnswer("");
-                return;
-            }
-            generateLevel(2);
-            setLevel(2);
-            setUserAnswer("");
-            return;
-        }
-        if (tries === 2) {
-            alert("You Lost, Try Again");
-            setTries(0);
-            setUserAnswer("");
-            generateLevel(1);
-            return;
-        }
-        alert("Incorrect, you have other oportunity");
-        setTries(tries + 1);
-    };
-
-    const fetchData = async () => {
-        try {
-            const result = await getData(urlApi + adjective);
-            return result;
-        } catch (error) {
-            console.error(error);
-        }
-    };
+    const [adjective, setAdjective] = useState(null);
+    const [userAnswer, setUserAnswer] = useState(null);
 
     useEffect(() => {
+        if (!adjective) return;
+
         const getAudio = async () => {
-            const apiData = await fetchData();
+            const apiData = await getData(urlApi + adjective || "");
             if (apiData && apiData.length > 0 && apiData[0].phonetics) {
                 const linkAudio = apiData[0].phonetics[0].audio;
                 playUriSound(linkAudio);
@@ -85,41 +37,83 @@ export default function FillInTheGaps() {
         getAudio();
     }, [adjective]);
 
+    useEffect(() => generateLevel(levels[0]), []);
+
     useEffect(() => {
-        generateLevel(1);
-    }, []);
+        if (tries > 0 && tries < maxTries)
+            alert(`Incorrect, you have ${maxTries - tries} oportunities left`);
+        if (tries === maxTries) return resetGame("You Lost, Try Again");
+    }, [tries]);
+
+    const randomNumber = (number) => {
+        return Math.floor(
+            Math.random() * levelTranslator[number].adjectives.length
+        );
+    };
+
+    const generateLevel = (number) => {
+        const random = randomNumber(number);
+        const levelData = levelTranslator[number];
+
+        setLevel(number);
+        setAdjective(levelData.adjectives[random]);
+        setSentence(levelData.sentences[random]);
+        setUserAnswer(null);
+    };
+
+    const resetAnswer = () => {
+        setTries((prev) => prev + 1);
+        setUserAnswer(null);
+    };
+
+    const resetGame = (alertMessage) => {
+        if (alertMessage) alert(alertMessage);
+
+        setTries(0);
+        generateLevel(levels[0]);
+    };
+
+    const correctAnswer = () => {
+        const isSuccess = adjective === userAnswer.toLowerCase();
+
+        if (!isSuccess) return resetAnswer();
+
+        if (isSuccess && level < levels[levels.length - 1])
+            return generateLevel(level + 1);
+
+        return resetGame("You WIN!!, Try Again");
+    };
 
     return (
         <View
             style={{
                 justifyContent: "center",
-                alignSelf: "center",
+                alignItems: "center",
                 marginVertical: 80,
+                display: "flex",
+                rowGap: 20,
             }}
         >
-            <View style={{ flexDirection: "row" }}>
-                <View style={{ padding: 2 }}>
-                    <Text
-                        style={{
-                            fontSize: 15,
-                            color: "black",
-                            fontWeight: "bold",
-                        }}
-                    >
-                        {sentence}
-                    </Text>
-                </View>
-            </View>
+            <Text
+                style={{
+                    fontSize: 15,
+                    color: "black",
+                    fontWeight: "bold",
+                }}
+            >
+                {sentence}
+            </Text>
             <TextInput
                 style={{
-                    color: "white",
-                    borderRadius: 8,
+                    color: "black",
                     justifyContent: "center",
                     alignItems: "center",
                     textAlignVertical: "center",
-                    backgroundColor: "black",
-                    height: 80,
+                    height: 30,
                     width: 200,
+                    textAlign: "center",
+                    borderBottomColor: "black",
+                    borderBottomWidth: 2,
                 }}
                 onChangeText={(text) => setUserAnswer(text)}
                 value={userAnswer}
@@ -130,11 +124,12 @@ export default function FillInTheGaps() {
                     justifyContent: "center",
                     alignItems: "center",
                     textAlignVertical: "center",
-                    backgroundColor: "black",
-                    height: 80,
+                    backgroundColor: !userAnswer ? "grey" : "black",
+                    height: 60,
                     width: 200,
                 }}
                 onPress={correctAnswer}
+                disabled={!userAnswer}
             >
                 <Text style={{ fontSize: 20, color: "white" }}>Check!</Text>
             </TouchableOpacity>
