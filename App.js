8205f06bea85e6018fddefbc7e57afd3a3ac3012
DavidReinon <davidreinongarcia@gmail.com>
2024-01-16 14:11:39 +0100

    añadido React import

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer } from '@react-navigation/native';
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
 
-import Definitions from './Evaluable03/src/screens/tab/Definitions';
-import Listening from './Evaluable03/src/screens/tab/Listening';
-import FillInTheGaps from './Evaluable03/src/screens/tab/FillInTheGaps';
-import Synonyms from './Evaluable03/src/screens/tab/Synonyms';
+import Definitions from "./Evaluable03/src/screens/tab/Definitions";
+import Listening from "./Evaluable03/src/screens/tab/Listening";
+import FillInTheGaps from "./Evaluable03/src/screens/tab/FillInTheGaps";
+import Synonyms from "./Evaluable03/src/screens/tab/Synonyms";
 
 const Tab = createBottomTabNavigator();
 const App = () => (
-  <NavigationContainer>
-    <Tab.Navigator>
-      <Tab.Screen name="Definitions" component={Definitions} />
-      <Tab.Screen name="Listening" component={Listening} />
-      <Tab.Screen name="FillInTheGaps" component={FillInTheGaps} />
-      <Tab.Screen name="Synonyms" component={Synonyms} />
-    </Tab.Navigator>
-  </NavigationContainer>
+    <NavigationContainer>
+        <Tab.Navigator>
+            <Tab.Screen name="Definitions" component={Definitions} />
+            <Tab.Screen name="Listening" component={Listening} />
+            <Tab.Screen name="FillInTheGaps" component={FillInTheGaps} />
+            <Tab.Screen name="Synonyms" component={Synonyms} />
+        </Tab.Navigator>
+    </NavigationContainer>
 );
 export default App;
