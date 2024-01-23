153dbea6c39eb563e157bca58996ee9d3b7a3b0c
DavidReinon <davidreinongarcia@gmail.com>
2024-01-23 16:11:46 +0100

    init archivos ejercicio12

--- App.js
+++ App.js
@@ -2,19 +2,19 @@ import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 
-import Definitions from "./Evaluable03/src/screens/tab/Definitions";
-import Listening from "./Evaluable03/src/screens/tab/Listening";
-import FillInTheGaps from "./Evaluable03/src/screens/tab/FillInTheGaps";
-import Synonyms from "./Evaluable03/src/screens/tab/Synonyms";
+import Ejercicio1 from "./Ejercicios12Camara/src/screens/Ejercicio1";
+import Ejercicio2 from "./Ejercicios12Camara/src/screens/Ejercicio2";
+import Ejercicio3 from "./Ejercicios12Camara/src/screens/Ejercicio3";
+import Ejercicio4 from "./Ejercicios12Camara/src/screens/Ejercicio4";
 
 const Tab = createBottomTabNavigator();
 const App = () => (
     <NavigationContainer>
         <Tab.Navigator>
-            <Tab.Screen name="Definitions" component={Definitions} />
-            <Tab.Screen name="Listening" component={Listening} />
-            <Tab.Screen name="FillInTheGaps" component={FillInTheGaps} />
-            <Tab.Screen name="Synonyms" component={Synonyms} />
+            <Tab.Screen name="Ejercicio1" component={Ejercicio1} />
+            <Tab.Screen name="Ejercicio2" component={Ejercicio2} />
+            <Tab.Screen name="Ejercicio3" component={Ejercicio3} />
+            <Tab.Screen name="Ejercicio4" component={Ejercicio4} />
         </Tab.Navigator>
     </NavigationContainer>
 );
